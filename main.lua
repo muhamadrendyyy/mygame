315707cad1f686a4d3bff97bc1eafd60d436fb63
muhamadrendyyy <153420153+muhamadrendyyy@users.noreply.github.com>
2024-02-12 09:52:13 +0700

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,11 +1,19 @@
 function love.load()
+    anim8 = require 'libraries/anim8'
+
     player = {}
     player.x = 400
     player.y = 200
     player.speed = 5
     player.sprite = love.graphics.newImage('sprites/parrot.png')
+    player.spriteSheet = love.graphics.newImage('sprites/human.png')
+    player.grid = anim8.newGrid( 12, 18, player.spriteSheet:getWidth(), player.spriteSheet:getHeight() )
+
+    player.animations = {}
+    player.animations.down = anim8.newAnimation( player.grid('1-4', 1), 0.2)
 
     background = love.graphics.newImage('sprites/background.png')
+
 end
 
 function love.update(dt)
@@ -17,16 +25,18 @@ function love.update(dt)
         player.x = player.x - player.speed
     end
 
-    if love.keyboard.isDown("dowm") then
+    if love.keyboard.isDown("down") then
         player.y = player.y + player.speed
     end
 
     if love.keyboard.isDown("up") then
         player.y = player.y - player.speed
     end
+
+    player.animations.down:update(dt)
 end
 
 function love.draw()
     love.graphics.draw(background, 0, 0)
-   love.graphics.draw(player.sprite, player.x, player.y)
+    player.animations.down:draw(player.spriteSheet, player.x, player.y)
 end    
